c048af9c780baf3142257a6aab22c7efde7a3cc2
Sethumadhavan K <sethuksethu@gmail.com>
2020-04-20 11:25:33 +0530

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
    $('.loader').hide();
+
+   $('#mobile_menu').off('click').on('click',function(){
+      $('#side-bar').css('right','0px');
+   });
+
+   $('#menu_close').off('click').on('click',function(){
+      $('#side-bar').css('right','-350px');
+   })
 });
